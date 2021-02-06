d3a3397bb3738c31240fedf2019ca6d61a3b0b00
kyle(홍민기) <kyle.hong@kakaocorp.com>
2021-02-07 06:05:31 +0900

    output 파일 포멧 정리

--- base/keyword_search.py
+++ base/keyword_search.py
@@ -3,8 +3,9 @@ import sys
 import csv
 import time
 
-_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbklkIjoiaG9uZ19vd25lcjpuYXZlciIsInJvbGUiOjAsImNsaWVudElkIjoibmF2ZXItY29va2llIiwiaXNBcGkiOmZhbHNlLCJ1c2VySWQiOjIzNTU4NjIsInVzZXJLZXkiOiI3YzEyYmFjOC0xZjg5LTRkODQtODZiOC0zNGMwMGY5ZjljNmQiLCJjbGllbnRDdXN0b21lcklkIjoyMTA1NTE0LCJpc3N1ZVR5cGUiOiJ1c2VyIiwibmJmIjoxNjEyNjM5NTkzLCJpZHAiOiJ1c2VyLWV4dC1hdXRoIiwiY3VzdG9tZXJJZCI6MjEwNTUxNCwiZXhwIjoxNjEyNjQwMjUzLCJpYXQiOjE2MTI2Mzk2NTMsImp0aSI6ImU0OWE0Njg3LTA4MmEtNDdmMS05NTI4LTY1ZjA2NTU3ZjMyZCJ9.GO0JjJ6VlYAv7AmHycwDYFwU6Y7PVAX80jsOjEsr4Kk'
+_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpbklkIjoiaG9uZ19vd25lcjpuYXZlciIsInJvbGUiOjAsImNsaWVudElkIjoibmF2ZXItY29va2llIiwiaXNBcGkiOmZhbHNlLCJ1c2VySWQiOjIzNTU4NjIsInVzZXJLZXkiOiI3YzEyYmFjOC0xZjg5LTRkODQtODZiOC0zNGMwMGY5ZjljNmQiLCJjbGllbnRDdXN0b21lcklkIjoyMTA1NTE0LCJpc3N1ZVR5cGUiOiJ1c2VyIiwibmJmIjoxNjEyNjQ1MDA0LCJpZHAiOiJ1c2VyLWV4dC1hdXRoIiwiY3VzdG9tZXJJZCI6MjEwNTUxNCwiZXhwIjoxNjEyNjQ1NjY0LCJpYXQiOjE2MTI2NDUwNjQsImp0aSI6ImY4NjVhNGJhLTY5ZjMtNDQ5ZS04YWY3LTM2MGMwNjczZTdhMiJ9.0bVrfgS6AA5gEN--ZTbp6vHAdU4JhLO7mg_hoSPjKPI'
 _clientId = '-XfwsIC9THj27j7OVClGw'
+_keywordSet = set()
 
 def getClientId():
     clientId = input("ClientId 입력 : ")
@@ -14,26 +15,32 @@ def getBearerToken():
     global _token
     _token = input("베어러 토큰 입력 : ")
 
-def writeCsv():
-    f = open('example.csv','w', newline='')
-    wr = csv.writer(f)
-    wr.writerow(['탁자', 'a,b,c,d,e,f,g'])
-    wr.writerow(['탁자2', 'a,b,c,d,e,f,g'])
-    wr.writerow(['탁자3', 'a,b,c,d,e,f,g'])
-
-    f.close()
-
 def readCsv():
-    f = open('example.csv', 'r')
+    f = open('상품조사.csv', 'r')
     rdr = csv.reader(f)
     for line in rdr:
-        encodeKeyword(line[0])
-        encodeKeyword(line[1])
-    
+        product_name = line[0]
+        rel_keywords = line[1]
+        rel_keyword_list = rel_keywords.split(',')
+
+        keywords_cnt = len(rel_keyword_list)
+        keyword_grp = keywords_cnt // 5 + 1
+
+        ff = open('{}.csv'.format(product_name),'w',encoding= 'utf-8-sig', newline='')
+        wr = csv.writer(ff)
+        wr.writerow(['상품명','카테고리','검색수','상품수','경쟁률'])
+        ff.close()
+
+        for group_num in range(keyword_grp):
+            keyword_list_part = rel_keyword_list[group_num*5:group_num*5+5]
+            keyword_str = ','.join(keyword_list_part)
+            sendRequestToNaverKeywordTool(product_name, keyword_str)
+            print("---{}% done---".format((group_num + 1) * 100 // keyword_grp ))
+
     f.close()
 
 def filterKeywords(productName, keywordList):
-    f = open('{}.csv'.format(productName),'a', newline='')
+    f = open('{}.csv'.format(productName),'a',encoding= 'utf-8-sig', newline='')
     wr = csv.writer(f)
 
     for keywordObj in keywordList:
@@ -52,8 +59,6 @@ def filterKeywords(productName, keywordList):
     f.close()
 
 def sendRequestToNaverKeywordTool(productName= '', keywords=''):
-    keywordSet = set()
-
     bearerToken = 'Bearer ' + _token
     url = 'https://manage.searchad.naver.com/keywordstool'
 
@@ -77,13 +82,11 @@ def sendRequestToNaverKeywordTool(productName= '', keywords=''):
 
             keywordList = resp.json().get('keywordList')
 
-            print("검색된 단어 개수 : {}".format(len(keywordList)))
-
             parsedKeywordList = []
             for keyword in keywordList:
-                if keyword['relKeyword'] in keywordSet:
+                if keyword['relKeyword'] in _keywordSet:
                     continue
-                keywordSet.add(keyword['relKeyword'])
+                _keywordSet.add(keyword['relKeyword'])
 
                 if keyword['monthlyPcQcCnt'] == '< 10' :
                     keyword['monthlyPcQcCnt'] = 0
@@ -133,9 +136,6 @@ def sendRequestToNaverShopping(keyword):
 
 start_time = time.time()
 
-sendRequestToNaverKeywordTool("예쁜탁자", "탁자,1인용탁자")
-sendRequestToNaverKeywordTool("예쁜탁자2", "탁자,1인용탁자")
-sendRequestToNaverKeywordTool("예쁜탁자3", "탁자,1인용탁자")
-sendRequestToNaverKeywordTool("예쁜탁자4", "탁자,1인용탁자")
+readCsv()
 
 print("---Total time : {}---".format(time.time() - start_time))
