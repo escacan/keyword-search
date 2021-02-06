c5edb3faa8bcbdc5ede7e064387ce7ee62f6109f
kyle(홍민기) <kyle.hong@kakaocorp.com>
2021-02-07 00:00:16 +0900

    검색수로 필터링하는 함수 추가

--- base/keyword_search.py
+++ base/keyword_search.py
@@ -1,8 +1,6 @@
 import requests
 import sys
 import csv
-import urllib
-import urllib.parse 
 
 def getBearerToken():
     token = input("베어러 토큰 입력 : ")
@@ -26,10 +24,19 @@ def readCsv():
     
     f.close()
 
+def filterKeywords(keywordDict):
+    # 총 검색횟수 1000회 이상인 키워드 필터링
+    filterKeywordsDict = {}
+    for keyword, cnt in keywordDict.items():
+        if cnt >= 1000:
+            filterKeywordsDict[keyword] = cnt
+
+    print("검색수 1000이상인 키워드 개수 : ", len(filterKeywordsDict))
+
 def sendRequests(token, keywords=''):
     bearerToken = 'Bearer ' + token
     url = 'https://manage.searchad.naver.com/keywordstool'
-    print(url)
+
     headers = {'Authorization': bearerToken}
     params = {
         'format': 'json',
@@ -38,17 +45,49 @@ def sendRequests(token, keywords=''):
 
     try:
         resp = requests.get(url= url, headers= headers, params= params)
-        print(resp.json())
 
-        # print(len(keywords))
-    except urllib.error.HTTPError as e:
-        if e.getcode() == 401:
+        if resp.status_code == 401:
             print('Bearer토큰 유효시간 민료')
             newToken = getBearerToken()
-            sendRequests(newToken, keywords)
+            sendRequests(newToken, keywords)        
         else:
-            return
+            if resp.status_code != 200:
+                print("StatusCode가 이상하다. 확인필요")
+                return
 
+            keywordList = resp.json().get('keywordList')
+
+            print("검색된 단어 개수 : {}".format(len(keywordList)))
+
+            tempKeyword = keywordList[0]
+
+            print(tempKeyword)
+
+            print(type(tempKeyword['monthlyPcQcCnt']))
+
+            print(type(tempKeyword['monthlyMobileQcCnt']))
+
+            print(tempKeyword['monthlyPcQcCnt'] + tempKeyword['monthlyMobileQcCnt'])
+
+            parsedKeywordDict = {}
+            i = 0
+            try:
+                for keyword in keywordList:
+                    if keyword['monthlyPcQcCnt'] == '< 10' or keyword['monthlyMobileQcCnt'] == '< 10':
+                        i = i+1
+                        continue;
+
+                    parsedKeywordDict[keyword['relKeyword']] = keyword['monthlyPcQcCnt'] + keyword['monthlyMobileQcCnt']                
+                    i = i + 1
+            except Exception as e:
+                print(str(e))
+                print("Error on {}".format(i))
+                print(keywordList[i])
+
+            filterKeywords(parsedKeywordDict)
+
+    except Exception as e:
+        print(str(e))
 
 token = getBearerToken()
 sendRequests(token, "탁자,1인용탁자")
